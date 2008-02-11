9d0a1c883975a2bae28d728c8d399a495e97b27f
cnguyen <cnguyen>
2008-02-11 20:47:48 +0000

    Feb 04 Notes

---
+++ development/calls/02-04-08.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p><strong>Eclipse  Babel Project  Development Meeting: 2/04/08</strong><br />
+    <strong>Attendees:</strong></p>
+<ul type="disc">
+  <li>Chris Nguyen</li>
+  <li>Kit        Lo </li>
+  <li>Pat Huff</li>
+  <li>Antoine Toulme</li>
+  <li>Gabe</li>
+  <li>Denis</li>
+  <li>Pascal</li>
+  <li>Nigel        Westbury</li>
+  <li>Werner        Keil</li>
+</ul>
+<ol start="1" type="1">
+  <li>Jess Garms to Leave as an       Eclipse Committer</li>
+  <li>Server-side Translation       Project Update – Denis</li>
+  <ol start="1" type="1">
+    <li>Import on the UI is        almost done</li>
+    <ol start="1" type="1">
+      <li>Almost ready for the         General Public</li>
+      <li>Release “0” will be         for the public</li>
+      <ol start="1" type="1">
+        <li>We want something up          so we can get people to start translating</li>
+      </ol>
+    </ol>
+  </ol>
+  <li>EclipseCon</li>
+  <ol start="1" type="1">
+    <li>Just submitted a BOF        session – hopefully will be accepted soon</li>
+    <ol start="1" type="1">
+      <li>Introduce to the         Community</li>
+      <li>Collect some new         ideas/enhancements for the project</li>
+    </ol>
+    <li>Need to plan a working        session for our committers</li>
+    <ol start="1" type="1">
+      <li>Kit will get some         dates together</li>
+      <li>Possible Attendees:</li>
+      <ol start="1" type="1">
+        <li>Kit Lo</li>
+        <li>Jess Garms</li>
+        <li>Chris Nguyen</li>
+        <li>Denis</li>
+        <li>Gabe</li>
+        <li>Antoine</li>
+        <li>Hugh</li>
+        <li>Pat Huff</li>
+        <li>Pascal (tentative)</li>
+        <li>Chris to send out email          to Babel          to find a convenient time</li>
+      </ol>
+    </ol>
+    <li>2 Short talks accepted        (10 minutes each)</li>
+    <ol start="1" type="1">
+      <li>Introduction to the         Eclipse Babel         Project </li>
+      <ol start="1" type="1">
+        <li>Kit is going to          cover this</li>
+        <li>Might be right time          to introduce the message editor</li>
+        <ol start="1" type="1">
+          <li>Kit will reach out           to Nigel for more information</li>
+        </ol>
+      </ol>
+      <li>The Eclipse Babel Project         Translation Server</li>
+      <ol start="1" type="1">
+        <li>Will there be a demo          involved?</li>
+        <li>Please send Kit some          slides</li>
+        <li>Requested that short          talks be scheduled back to back</li>
+        <ol start="1" type="1">
+          <li>First session, talk</li>
+          <li>Session, demo</li>
+        </ol>
+      </ol>
+    </ol>
+  </ol>
+  <li>Copyright information – Kit Lo</li>
+  <ol start="1" type="1">
+    <li>No new information</li>
+    <li>We may want to include        some type of comment to show an audit trail</li>
+    <ol start="1" type="1">
+      <li>Bjorn is working on         the message file</li>
+      <li>We currently do         record who contributes the strings and do have some sort of tracking</li>
+      <ol start="1" type="1">
+        <li>The version that          will not be released this week will not have the tracking.  However, moving forward, we will          include this</li>
+      </ol>
+    </ol>
+  </ol>
+  <li>Additional Topics</li>
+  <ol start="1" type="1">
+    <li>Face to face        opportunity for the Mylyn project</li>
+  </ol>
+</ol>
+<p>&nbsp;</p>
+</body>
+</html>
