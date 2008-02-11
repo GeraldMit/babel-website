4e7543c6694684aa0928c50477afd160751a2216
cnguyen <cnguyen>
2008-02-11 21:09:53 +0000

    *** empty log message ***

---
+++ development/calls/02-11-08.php
@@ -0,0 +1,69 @@
+<p><strong>Eclipse  Babel Project  Development Meeting: 2/11/08</strong><br />
+    <strong>Attendees:</strong></p>
+<ul type="disc">
+  <li>Chris Nguyen</li>
+  <li>Kit        Lo </li>
+  <li>Pat Huff</li>
+  <li>Werner        Keil</li>
+  <li>Denis</li>
+  <li>Gabe</li>
+</ul>
+<ol start="1" type="1">
+  <li>Jess Garms to Leave as an       Eclipse Committer</li>
+  <li>Server-side Translation       Project Update – Denis</li>
+  <ol start="1" type="1">
+    <li>Tried importing the        3.2 translated strings</li>
+    <li>Had discussions w/ Kit        and should be Escape Unicode as that is how they are formatted now</li>
+  </ol>
+  <li>EclipseCon</li>
+  <ol start="1" type="1">
+    <li>Working on        presentation deadline by Sunday </li>
+    <ol start="1" type="1">
+      <li>We need to send it in         for printing/loading onto conference memory sticks</li>
+      <li>Once he’s done we can         provide feedback</li>
+    </ol>
+    <li>Just submitted a BOF        session – accepted</li>
+    <ol start="1" type="1">
+      <li>Introduce to the         Community</li>
+      <li>Collect some new         ideas/enhancements for the project</li>
+    </ol>
+    <li>Need to plan a working        session for our committers</li>
+    <ol start="1" type="1">
+      <li>We will meet at the         hotel bar at 5pm on Monday to get acquainted with one another and will         decide on meeting for a longer Working session that week</li>
+      <li>Possible Attendees:</li>
+      <ol start="1" type="1">
+        <li>Kit Lo</li>
+        <li>Jess Garms</li>
+        <li>Chris Nguyen</li>
+        <li>Denis</li>
+        <li>Gabe</li>
+        <li>Antoine</li>
+        <li>Hugh</li>
+        <li>Pat Huff</li>
+        <li>Pascal (tentative)</li>
+        <li>Chris to send out          email to Babel          to find a convenient time</li>
+      </ol>
+    </ol>
+  </ol>
+  <li>We need to come up Critical       Bug list to entice people to join Babel</li>
+  <ol start="1" type="1">
+    <li>Bug List (WIP):</li>
+    <ol start="1" type="1">
+      <li>Must finish 3.2         translations into the server before EclipseCon</li>
+      <li>There are duplicating         languages in the list that we need to fix</li>
+    </ol>
+  </ol>
+  <li>We need to have a release       plan:</li>
+  <ol start="1" type="1">
+    <li>Should we have a        release number?</li>
+    <li>Kit and Chris to        discuss after the call</li>
+    <li>In order to do a        release, you have to have approval from the EMO and we should have a        release review</li>
+    <ol start="1" type="1">
+      <li>Need to discuss this         during EclipseCon</li>
+      <li>ACTION: put a link to         point to release review (Kit to send to Chris)</li>
+      <li>Guidelines for Release Reviews<br />
+        <a href="http://www.eclipse.org/projects/dev_process/release-review.php">http://www.eclipse.org/projects/dev_process/release-review.php</a></li>
+    </ol>
+  </ol>
+</ol>
+<p>&nbsp;</p>
