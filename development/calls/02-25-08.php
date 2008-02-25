3176b0e49b987c343334ed1997680d1624627434
cnguyen <cnguyen>
2008-02-25 18:33:58 +0000

    *** empty log message ***

---
+++ development/calls/02-25-08.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p><strong>Eclipse  Babel Project  Development Meeting: 2/25/08</strong><br />
+    <strong>Attendees:</strong></p>
+<ul type="disc">
+  <li>Chris Nguyen</li>
+  <li>Kit        Lo </li>
+  <li>Denis</li>
+  <li>Pascal</li>
+  <li>Daniel McGowan</li>
+  <li>Werner</li>
+  <li>Gabe</li>
+</ul>
+<ol start="1" type="1">
+  <li>Server-side Translation       Project Update (Babel Translations Server) – Denis</li>
+  <ol start="1" type="1">
+    <li>Ready to put 3.2        translations into server but testing before doing so</li>
+    <li>Testing 3.2 versus 3.1</li>
+    <li>Also needs to lobby        all the projects to define the map files (not high priority)</li>
+    <li>Once imported, Denis        will need help building the language pack</li>
+  </ol>
+  <li>EclipseCon</li>
+  <ol start="1" type="1">
+    <li>Kit Submitted        presentations on time and will be on conference memory stick to make        changes</li>
+    <ol start="1" type="1">
+      <li>Please send Kit back         feedback</li>
+      <li><a href="https://eclipsecon.greenmeetingsystems.com/attachments/download/80">https://eclipsecon.greenmeetingsystems.com/attachments/download/80</a></li>
+      <li><a href="https://eclipsecon.greenmeetingsystems.com/attachments/download/82">https://eclipsecon.greenmeetingsystems.com/attachments/download/82</a></li>
+    </ol>
+    <li>Working Session</li>
+    <ol start="1" type="1">
+      <li>We will meet at the         hotel bar at 5pm on Monday to get acquainted with one another and will         decide on meeting for a longer Working session that week</li>
+      <li>Possible Attendees:</li>
+      <ol start="1" type="1">
+        <li>Kit Lo</li>
+        <li>Chris Nguyen</li>
+        <li>Denis</li>
+        <li>Gabe</li>
+        <li>Antoine</li>
+        <li>Hugh</li>
+        <li>Pat Huff</li>
+        <li>Pascal (tentative)</li>
+      </ol>
+    </ol>
+  </ol>
+  <li>We need to come up Critical       Bug list to entice people to join Babel</li>
+  <ol start="1" type="1">
+    <li>Bug List (WIP):</li>
+    <ol start="1" type="1">
+      <li>Must finish 3.2         translations into the server before EclipseCon</li>
+    </ol>
+  </ol>
+  <li>Difference between Editors:</li>
+  <ol start="1" type="1">
+    <li>Pascal's editor in the        IDE - 'Messages Editor'</li>
+    <ol start="1" type="1">
+      <li>Switched it to         Message editor when he contributed it</li>
+      <li>Wants to eventually         make it support more than just property files (xml, etc)</li>
+    </ol>
+    <li>Nigel's editor in        runtime - 'Runtime Translation Editor'</li>
+  </ol>
+  <li>We need to have a release       plan:</li>
+  <ol start="1" type="1">
+    <li>Our project will have        issues because we’re dealing w/ translations</li>
+    <ol start="1" type="1">
+      <li>I.e. At GA time we         may have 80% of the translations times or 90% a month later, etc.</li>
+      <li>Kit will discuss with         Bjorn for more advice</li>
+      <li>Need to discuss this         during EclipseCon</li>
+      <li>Guidelines for Release Reviews<br />
+        <a href="http://www.eclipse.org/projects/dev_process/release-review.php">http://www.eclipse.org/projects/dev_process/release-review.php</a></li>
+    </ol>
+  </ol>
+</ol>
+<p>&nbsp;</p>
+</body>
+</html>
