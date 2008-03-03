d09425dd9cfac1c8ca134bb2da41b6b160123359
cnguyen <cnguyen>
2008-03-03 18:10:05 +0000

    *** empty log message ***

---
+++ development/calls/03-03-08.php
@@ -0,0 +1,43 @@
+<p><strong>Eclipse  Babel Project  Development Meeting: 3/03/08</strong><br />
+    <strong>Attendees:</strong></p>
+<ul type="disc">
+  <li>Chris Nguyen</li>
+  <li>Kit        Lo </li>
+  <li>Gabe</li>
+</ul>
+<ol start="1" type="1">
+  <li>Server-side Translation       Project Update (Babel Translations Server) –Gabe</li>
+  <ol start="1" type="1">
+    <li>Closing bugs that Kit        filed</li>
+    <li>Just a few more issues        – Gabe will build a server tool for bug posting (screen shots)</li>
+  </ol>
+  <li>EclipseCon</li>
+  <ol start="1" type="1">
+    <li>Import for 3.2        translations are on the server</li>
+    <li>Kit Submitted        presentations on time and will be on conference memory stick to make        changes</li>
+    <ol start="1" type="1">
+      <li>Please send Kit back         feedback</li>
+      <li><a href="https://eclipsecon.greenmeetingsystems.com/attachments/download/80">https://eclipsecon.greenmeetingsystems.com/attachments/download/80</a></li>
+      <li><a href="https://eclipsecon.greenmeetingsystems.com/attachments/download/82">https://eclipsecon.greenmeetingsystems.com/attachments/download/82</a></li>
+    </ol>
+    <li>Working Session</li>
+    <ol start="1" type="1">
+      <li>We will meet at the         hotel bar at 5pm on Monday to get acquainted with one another and will         decide on meeting for a longer Working session that week</li>
+    </ol>
+  </ol>
+  <li>We need to have a release       plan:</li>
+  <ol start="1" type="1">
+    <li>Need to talk to Bjorn        about this during our working session</li>
+    <ol start="1" type="1">
+      <li>Gabe will talk to him         to see when he’d like to discuss this topic</li>
+    </ol>
+    <li>Our project will have        issues because we’re dealing w/ translations</li>
+    <ol start="1" type="1">
+      <li>I.e. At GA time we         may have 80% of the translations times or 90% a month later, etc.</li>
+      <li>Kit will discuss with         Bjorn for more advice</li>
+      <li>Need to discuss this         during EclipseCon</li>
+      <li>Guidelines for Release Reviews<br />
+        <a href="http://www.eclipse.org/projects/dev_process/release-review.php">http://www.eclipse.org/projects/dev_process/release-review.php</a></li>
+    </ol>
+  </ol>
+</ol>
