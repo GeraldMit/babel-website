14d1e076bcd469dd63730c47e3bb1ade7b2d2de0
cnguyen <cnguyen>
2008-03-10 17:29:52 +0000

    *** empty log message ***

---
+++ development/calls/03-10-08.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p><strong>Eclipse  Babel Project  Development Meeting: 3/10/08</strong><br />
+    <strong>Attendees:</strong></p>
+<ul type="disc">
+  <li>Chris Nguyen</li>
+  <li>Kit        Lo </li>
+  <li>Gabe</li>
+  <li>Daniel McGowan</li>
+  <li>Margaret Wong</li>
+  <li>Nigel Westbury</li>
+  <li>Kit Lo</li>
+  <li>Pat Huff</li>
+</ul>
+<ul>
+<ul>
+  <li>Server-side Translation Project Update (Babel  Translations Server) –Gabe</li>
+</ul>
+<ul>
+  <li>Closing bugs that Kit filed</li>
+  <li>Just a few more issues – Gabe will build a server tool  for bug posting (screen shots)</li>
+</ul>
+<ul>
+  <li>EclipseCon</li>
+</ul>
+<ul>
+  <li>Import for 3.2 translations are on the server</li>
+  <li>Minor bugs on the translation server</li>
+  <li>Kit Submitted presentations on time and will be on  conference memory stick to make changes</li>
+  <ul>
+    <li>Please send Kit back feedback</li>
+    <li><a href="https://eclipsecon.greenmeetingsystems.com/attachments/download/80">https://eclipsecon.greenmeetingsystems.com/attachments/download/80</a></li>
+    <li><a href="https://eclipsecon.greenmeetingsystems.com/attachments/download/82">https://eclipsecon.greenmeetingsystems.com/attachments/download/82</a></li>
+  </ul>
+  <li>Working Session</li>
+  <ul>
+    <li>We will meet at the hotel bar at 5pm on Monday to get  acquainted with one another and will decide on meeting for a longer Working  session that week</li>
+  </ul>
+</ul>
+<ul>
+  <li>Cancel next week’s call (3/17/08)</li>
+  <li>We need to have a release plan:</li>
+</ul>
+<ul>
+  <li>Need to find out how the translations got imported</li>
+  <ul>
+    <li>Do we attach the translations to a bug or do we import  them?</li>
+    <li>Might need to get an IP clearance (to have legal stuff  covered)</li>
+    <ul>
+      <li>This should be done before</li>
+      <li>We need to check the 3.2 translations that IBM donated</li>
+      <li>Gabe will check if Bjorn can attend our working session  next week</li>
+    </ul>
+  </ul>
+  <li>Need to talk to Bjorn about this during our working  session</li>
+  <ul>
+    <li>Gabe will talk to him to see when he’d like to discuss  this topic</li>
+  </ul>
+  <li>Our project will have issues because we’re dealing w/  translations</li>
+  <ul>
+    <li>I.e. At GA time we may have 80% of the translations  times or 90% a month later, etc.</li>
+    <li>Kit will discuss with Bjorn for more advice</li>
+    <li>Need to discuss this during EclipseCon</li>
+    <li>Guidelines for Release Reviews<br />
+      <a href="http://www.eclipse.org/projects/dev_process/release-review.php">http://www.eclipse.org/projects/dev_process/release-review.php</a></li>
+  </ul>
+</ul>
+<ul>
+  <li>Marge from Adobe – Welcome</li>
+</ul>
+<ul>
+<ul>
+  <li>Can each individual company still copyright their  strings (code contributions model)</li>
+  <li>Timeframe on Milestone 1 – goal is to get most of  things cleaned up by EclipseCon</li>
+</ul>
+<ul>
+<ul>
+  <li>Will the translation server process get changed?</li>
+</ul>
+<ul>
+<ul>
+  <li>potentially create 2 dumps</li>
+</ul>
+</body>
+</html>
