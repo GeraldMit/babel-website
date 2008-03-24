b251a6ea8ced2a517571b5ef397e214485b2a0a5
cnguyen <cnguyen>
2008-03-24 20:04:49 +0000

    *** empty log message ***

--- development/calls/03-24-08.php
+++ development/calls/03-24-08.php
@@ -1,36 +1,47 @@
-<p><strong>Eclipse  Babel Project  Meeting at EclipseCon: 3/24/08</strong><br>
-    <strong>Attendees:</strong></p>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<p>Eclipse  Babel Project  Meeting at EclipseCon: 3/24/08</p>
+<p>Attendees:</p>
 <ul type="disc">
   <li>Chris Nguyen</li>
   <li>Kit        Lo </li>
   <li>Pat Huff</li>
 </ul>
+<p>&nbsp;</p>
 <ul>
 <ul>
-  <li>Goal: Outreach to China  - Trying to find working groups in China (largest downloads) – talk to  Japanese contributors</li>
+  <li>Goal: Outreach to China  - Trying to find working groups in China (largest downloads) – talk to  Japanese contributors
+    <ul>
+      <li>Ask them for people that might be interested in  contributing</li>
+      <li>Attend Hong Kong Open source forum/conference</li>
+      <li>Reach out to Japanese IBM and BEA</li>
+    </ul>
+  </li>
+
+  <li>EclipseCon
+    <ul>
+      <li>Kit and Chris to create an EclipseCon report this week</li>
+    </ul>
+  </li>
+
+  <li>Large donations to Babel – How should we handle those?
+    <ul>
+      <li>Next step is for Kit to convert it into an NL pack and  pass it on to Denis for import – in another day or so</li>
+    </ul>
+  </li>
+  
+  <li>Topics for next week:
+    <ul>
+      <li>EclipseCon Working session topics</li>
+      <li>BOF topics</li>
+    </ul>
+  </li>
 </ul>
-<ul>
-  <li>Ask them for people that might be interested in  contributing</li>
-  <li>Attend Hong Kong Open source forum/conference</li>
-  <li>Reach out to Japanese IBM and BEA</li>
-</ul>
-<ul>
-  <li>EclipseCon</li>
-</ul>
-<ul>
-  <li>Kit and Chris to create an EclipseCon report this week</li>
-</ul>
-<ul>
-  <li>Large donations to Babel – How should we handle those?</li>
-</ul>
-<ul>
-  <li>Next step is for Kit to convert it into an NL pack and  pass it on to Denis for import – in another day or so</li>
-</ul>
-<ul>
-  <li>Topics for next week:</li>
-</ul>
-<ul>
-<ul>
-  <li>EclipseCon Working session topics</li>
-  <li>BOF topics</li>
-</ul>
+</body>
+</html>
