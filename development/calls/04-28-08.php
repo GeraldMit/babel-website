3de46fd305624b0537bf133459f1df7f2416115d
cnguyen <cnguyen>
2008-04-28 20:07:31 +0000

    *** empty log message ***

---
+++ development/calls/04-28-08.php
@@ -0,0 +1,54 @@
+
+<p>April 28, 2008 Babel  Eclipse Meeting</p>
+<p>Attendees: <br />
+  - Chris<br />
+  - Kit  <br />
+  - Margaret <br />
+  - Jeff <br />
+  - Gabe <br />
+  - Pascal<br />
+  - Daniel<br />
+  - Pat <br />
+  - Denis</p>
+<ul>
+<ul>
+  <li>Link for the Babel  project is the on the Eclipse Download Page</li>
+  <li>Everything is in Sync from the Language ID’s</li>
+  <li>Gabe is waiting for Denis to update the server so Babel has a “Needs help”  form</li>
+  <li>Pseudo Language:
+    <ul>
+      <li>Kit  listed some dependencies that Denis can’t get to at the moment</li>
+      <li>Still  not completed</li>
+    </ul>
+  </li>
+
+  <li>Questions:
+    <ul>
+      <li>Main  page on translated page is for everything – not version specific
+        <ul>
+          <li>A dumb # for everything</li>
+          <li>Combines all the translations</li>
+        </ul>
+      </li>
+      <li>How  far along are we from building a complete 3.3 language packs?</li>
+      <ul>
+        <li>All we need is a bit of code</li>
+        <li>Denis will take an action item to run some  queries to come up with a percentage complete so we have some stats on this
+                <ul>
+                  <li>Gabe will do the interface once Denis creates  the queries</li>
+                </ul>
+        </li>
+      </ul>
+    </ul>
+  </li>
+  <ul>
+    <li>What  web solutions are we using?  
+      <ul>
+        <li>'Denis: His resolutions is 1024x768</li>
+        <li>Page is a bit narrow</li>
+        <li>Denis could add a column/progress monitor after  he runs the queries</li>
+        <li>We don’t want to clutter up the page</li>
+      </ul>
+    </li>
+  </ul>
+</ul>
