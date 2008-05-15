92a41648eb36ee3e5d5360322c1e59b71242fac4
kitlo <kitlo>
2008-05-15 13:58:45 +0000

    *** empty log message ***

--- development/calls/05-05-08.php
+++ development/calls/05-05-08.php
@@ -1,8 +1,8 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-	$html = <<<EOHTML
 	$pageTitle		= "Eclipse Babel Project Development Meeting: 05/05/2008";
 	$pageKeywords	= "babel,eclipse,translation,i18n,l10n,internationalization,localization,globalization";
 	$pageAuthor		= "Kit Lo";
+	$html = <<<EOHTML
 	<div id="maincontent">
 		<div id="midcolumn">
 			<h1>$pageTitle</h1>
