aada0d3abfd05bda856109240a9ce09c71156769
kitlo <kitlo>
2008-08-04 19:39:58 +0000

    *** empty log message ***

--- development/calls/08-04-08.php
+++ development/calls/08-04-08.php
@@ -1,5 +1,5 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-	$pageTitle		= "Eclipse Babel Project Development Meeting: 07/28/2008";
+	$pageTitle		= "Eclipse Babel Project Development Meeting: 08/04/2008";
 	$pageKeywords	= "babel,eclipse,translation,i18n,l10n,internationalization,localization,globalization";
 	$pageAuthor		= "Kit Lo";
 	$html = <<<EOHTML
@@ -9,29 +9,22 @@
 			<h2>Attendees:</h2>
 			<ul>
 				<li>Kit</li>
+				<li>Pat</li>
 				<li>John</li>
 				<li>Matt</li>
 				<li>Karl</li>
-				<li>Denis</li>
 				<li>Gabe</li>
 				<li>Margaret</li>
+				<li>Jeff</li>
 			</ul>
 			<h2>Minutes:</h2>
 			<ol>
-				<li>Kit: I have a design question related to "possibly incorrect translations" (bug 230949).</li> 
-				<li>Changed English strings, for example, words being capitalized or changed to plural, will cause translations to be "possibly incorrect".</li> 
-				<li>If the translation actually needs to be changed, then it's easy. Translator will update the translation and save.</li>
-				<li>However, if translation does not need to be changed (for example, double-byte languages do not have plural numbers, translation does not need to be changed), do we need a "confirm" button?</li>
-				<li>If anyone has any idea about the design of the UI, please let Kit know.</li>
-				<li>Kit: I discovered some Simplified Chinese translations in the Traditional Chinese folder by one user.</li>
-				<li>Not sure if it was just a mistake or not.</li>
-				<li>Sent an e-mail to the user to confirm about 2 weeks ago, but did not receive any response.</li>
-				<li>Don't want to ban the user because it could be just a careless mistake.</li>
-				<li>Denis: Bugzilla has a function to temporarily suspend an ID with a note.</li>
-				<li>We could add a note and ask the user to contact us.</li>
-				<li>Kit: That sounds like a good function. I will collect more info and work with Denis off-line on that.</li>
-				<li>Gabe: We probably should implement a filter to show all translations from a user.</li>
-				<li>Kit: That's a useful function for language champions. Let's open a bug for that.</li>   
+				<li>Kit: Denis, Matt and I fixed a few bugs last week. Denis is on vacation this week. Is he the only one who could deploy the fixes to the live server?</li> 
+				<li>Gabe: Yes, he is the one who can do that.</li> 
+				<li>Kit: Okay, we will wait.</li>
+				<li>Kit: Strings marked as "non-translatable" have no history. We cannot tell who did that and when.</li>
+				<li>Gabe: We currently just mark the strings as "non-translatable" in the strings table. That's a good point. Recording the who and when info will be helpful.</li>
+				<li>Kit: I will check if we have a bug opened for that. I will open one if we don't have one opened.</li>
 				<li>Status of "critical bugs for Ganymede": (no change)</li> 
 				<ul>
 					<li><del>235132 - translations in some language packs do not show up because of extra folder name in language packs</del> - dup of 221675</li> 
@@ -43,7 +36,7 @@
 					<li><del>237275 - Update map files for Eclipse 3.4</del> - resolved</li>
 					<li>238587 - generate proper host-fragment tolerance range for language fragments - Kit will take a look</li>
 				</ul>
-				<li>Status of translation contributions":</li> 
+				<li>Status of translation contributions": (no change)</li> 
 				<ul>
 					<li>Actuate's BIRT 2.3 Translation for 6 languages - passed IP, imports reviewed by Denis, Kit, Doshiro-san. JWG did not respond to review request yet. We think these translations should be in good quality. We will go ahead and release them to the server.</li> 
 					<li>JWG's contributions - waiting for NEC to sign the Eclipse Employer Consent form.</li>
