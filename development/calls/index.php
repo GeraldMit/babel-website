48d13d8b7ce593eee2fc7be58eb0b2aa329125fa
kitlo <kitlo>
2008-09-11 17:15:48 +0000

    *** empty log message ***

--- development/calls/index.php
+++ development/calls/index.php
@@ -41,6 +41,7 @@
 		<h2>Call Archive</h2>
 		<ul>
 			<li>September 1, 2008 - meeting cancelled</li>
+			<li><a href="09-08-08.php">September 9, 2008</a></li>
 			<li><a href="08-25-08.php">August 25, 2008</a></li>
 			<li><a href="08-11-08.php">August 11, 2008</a></li>
 			<li><a href="08-04-08.php">August 4, 2008</a></li>
