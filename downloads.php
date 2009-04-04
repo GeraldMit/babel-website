b9016053d40d725fe2244f1c7d249140527379ee
kitlo <kitlo>
2009-04-04 01:45:54 +0000

    Add link for Galileo update site

--- downloads.php
+++ downloads.php
@@ -63,7 +63,7 @@ ol#known-issues ul li{
 		<h1>$pageTitle</h1>
 		<p>The Babel Project is in incubation and has not yet produced full translations for Eclipse Ganymede and Europa. All downloads are provided under the terms and conditions of the <a href="http://www.eclipse.org/legal/epl/notice.php" target="_blank">Eclipse 
           Foundation Software User Agreement</a> unless otherwise specified.</p>
-		<p>The Babel project produces weekly language packs from the translations entered using the Translation Tool. You can use the Babel Language Pack Update Sites to download the languages packs, or download the Babel Language Pack zips from the following link.</p>
+		<p>The Babel project produces weekly language packs from the translations entered using the Translation Tool. You can use the Babel Language Pack Update Sites to download the languages packs, or download the Babel Language Pack Zips from the following link.</p>
 
 		<p><b>Babel Language Packs built on $build_date</b></p>
 
@@ -79,7 +79,7 @@ ol#known-issues ul li{
 		Includes Eclipse SDK 3.3 &#160;<a href="http://babel.eclipse.org/babel/importing.php">[Request more projects]</a><br />
 		<font size="+1">http://download.eclipse.org/technology/babel/update-site/europa</b></font></td></tr></table></p>
 
-		<p><table><tr><td><img src="http://dev.eclipse.org/large_icons/actions/go-bottom.png" /></td><td><font size="+1"><b>Babel Language Pack zips</b></font></a><br />
+		<p><table><tr><td><img src="http://dev.eclipse.org/large_icons/actions/go-bottom.png" /></td><td><font size="+1"><b>Babel Language Pack Zips</b></font></a><br />
 		Includes Eclipse Galileo, Ganymede and Europa<br />
 		<a href="http://download.eclipse.org/technology/babel/babel_language_packs">
 		<font size="+1"><b>http://download.eclipse.org/technology/babel/babel_language_packs</b></font>
