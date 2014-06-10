3e3b101c17dcf7bc709ca98eef4a3c68a9f948d5
droy <denis.roy@eclipse.org>
2014-06-10 13:11:31 -0400

    Revert test theme.
    Signed-off-by: droy <denis.roy@eclipse.org>

--- _projectCommon.php
+++ _projectCommon.php
@@ -1,11 +1,5 @@
 <?php
 
-	# Set the theme for your project's web pages.
-	# See the Committer Tools "How Do I" for list of themes
-	# https://dev.eclipse.org/committers/
-	# Optional: defaults to system theme 
-	$theme = "solstice";
-
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
